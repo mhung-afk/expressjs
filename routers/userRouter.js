35eb59655d14511b6bf2f7ee904b9faf4eb2bb60
mhung-afk <rairaikiri@gmail.com>
2024-06-07 21:42:40 +0700

    get list user endpoint

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -5,9 +5,55 @@ import User from '../models/User.js';
 const userRouter = Router()
 
 // GET list of users
+/**
+ * sort name
+ * limit
+ * paging
+ * search by keyword
+ * group user based on year of birthday
+ */
 userRouter.get('/', async (req, res) => {
+    const { sortName, sortEmail, limit=4, page=1, keyword, year } = req.query;
     try {
-        const users = await User.find()
+        // sort
+        // const users = await User.find()
+        // .sort(sortName)
+
+        // .sort({
+        //     email: sortEmail === '-email' ? -1 : 1, // sắp trên => sort trước
+        //     name: sortName === '-name' ? -1 : 1,
+        // })
+
+        // .sort('name') // '-name'
+
+        // .sort({
+        //     name: 'asc' // 'ascending', 1
+        //     // name: 'desc' // 'descending', -1
+        // })
+
+        // limit
+        // const users = await User.find().limit(limit)
+
+        // paging
+        // const numUsers = await User.find().count()
+        // const numPages = (numUsers % limit === 0) ? (numUsers / limit) : (numUsers / limit + 1)
+        // const users = await User.find().skip(limit * (page - 1)).limit(limit)
+        // res.status(200).json({ users, numPages })
+
+        // search
+        // let keywordCondition = keyword ? 
+        // {
+        //     $or: [
+        //         {name: { $regex: keyword, $options: 'i' }}, // 'i' : insensitive
+        //         {email: { $regex: keyword, $options: 'i' }}
+        //     ]
+        // }
+        // : {}
+        // const users = await User.find(keywordCondition)
+
+        const users = await User.find({
+            birthday: '' /* năm === year */
+        })
         res.status(200).json(users)
     } catch (error) {
         console.log(error)
